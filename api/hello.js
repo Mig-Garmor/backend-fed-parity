8fbd355cc302ea9a20974058c441bdcbf6b0f090
Mig-Garmor <thecodermiguel@gmail.com>
2025-08-06 13:42:13 +0200

    Update

--- api/hello.js
+++ api/hello.js
@@ -4,8 +4,7 @@ export const config = {
 
 export default function handler(req, res) {
   if (req.method === "POST") {
-    const { name } = req.body;
-    res.status(200).json({ message: `Hello, ${name}` });
+    res.status(200).json({ message: `Hello world` });
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
