9aa650fb3f0ad9e4c885332737cfe8eb0b48e3ff
Mig-Garmor <thecodermiguel@gmail.com>
2025-08-11 00:29:33 +0200

    Updates to liquidity pair fetch

--- api/liquidity-pairs.js
+++ api/liquidity-pairs.js
@@ -9,60 +9,50 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+  if (!verifyApiKey(req)) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
   try {
-    // if (!verifyApiKey(req)) {
-    //   return res.status(401).json({ error: "Unauthorized" });
-    // }
+    const res = await fetch(
+      `https://api.dexscreener.com/token-pairs/v1/pulsechain/${token.pairAddress}`
+    );
+    const pairs = await res.json();
+    console.log("TOKEN PAIRS: ", pairs);
+    const pairsFormattedData = pairs.map((pair) => ({
+      pairAddress: pair.pairAddress,
+      baseToken: {
+        address: pair.baseToken.address,
+        symbol: pair.baseToken.symbol,
+      },
+      quoteToken: {
+        address: pair.quoteToken.address,
+        symbol: pair.quoteToken.symbol,
+      },
+      volume: {
+        h24: pair.volume.h24,
+      },
+      liquidity: {
+        total: pair.liquidity.usd,
+        baseToken: pair.liquidity.base,
+        quoteToken: pair.liquidity.quote,
+      },
+      pairCreationTime: pair.pairCreatedAt,
+    }));
+    // console.log("PAIRS FORMATTED DATA: ", pairsFormattedData);
+    // const pair = data.pairs?.[0];
 
-    const prices = {};
-
-    for (const token of TOKENS) {
-      try {
-        const res = await fetch(
-          `https://api.dexscreener.com/token-pairs/v1/pulsechain/${token.pairAddress}`
-        );
-        const pairs = await res.json();
-        console.log("TOKEN PAIRS: ", pairs);
-        const pairsFormattedData = pairs.map((pair) => ({
-          pairAddress: pair.pairAddress,
-          baseToken: {
-            address: pair.baseToken.address,
-            symbol: pair.baseToken.symbol,
-          },
-          quoteToken: {
-            address: pair.quoteToken.address,
-            symbol: pair.quoteToken.symbol,
-          },
-          volume: {
-            h24: pair.volume.h24,
-          },
-          liquidity: {
-            total: pair.liquidity.usd,
-            baseToken: pair.liquidity.base,
-            quoteToken: pair.liquidity.quote,
-          },
-          pairCreationTime: pair.pairCreatedAt,
-        }));
-        console.log("PAIRS FORMATTED DATA: ", pairsFormattedData);
-        // const pair = data.pairs?.[0];
-
-        // prices[token.name] = {
-        //   price: pair?.priceUsd || null,
-        //   lastUpdated: new Date().toISOString(),
-        // };
-      } catch (err) {
-        // prices[token.name] = {
-        //   error: true,
-        //   message: err.message,
-        //   lastUpdated: new Date().toISOString(),
-        // };
-      }
-    }
-
-    // await redis.set("tokenPrices", JSON.stringify(prices));
-
-    // return res.status(200).json({ message: "Prices updated", prices });
+    // prices[token.name] = {
+    //   price: pair?.priceUsd || null,
+    //   lastUpdated: new Date().toISOString(),
+    // };
+    return pairsFormattedData;
   } catch (err) {
-    // return res.status(500).json({ error: err.message });
+    // prices[token.name] = {
+    //   error: true,
+    //   message: err.message,
+    //   lastUpdated: new Date().toISOString(),
+    // };
+    return res.status(500).json({ error: err.message });
   }
 }
